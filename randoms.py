05f0ce7e854f6be835b2ff42911bd52d86e232d8
etetteh <cutetetteh@gmail.com>
2022-03-20 22:00:02 -0400

    Add random seed control file

---
+++ randoms.py
@@ -0,0 +1,13 @@
+import torch
+import random
+import numpy as np
+
+def set_seed(seed: int = 6):
+    """
+    This function controls sources of randomness to aid reproducibility.
+    Args:
+        seed int: to initialize random number generator (RNG)
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
